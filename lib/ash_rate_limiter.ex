5d3c55ad6b0a9191cde8965607f497e31897fca4
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-02 07:09:12 +0100

    chore: SPDX contributor links (#59)

--- lib/ash_rate_limiter.ex
+++ lib/ash_rate_limiter.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 ash_rate_limiter contributors <https://github.com/ash-project/ash_rate_limiter/graphs.contributors>
+# SPDX-FileCopyrightText: 2025 ash_rate_limiter contributors <https://github.com/ash-project/ash_rate_limiter/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
